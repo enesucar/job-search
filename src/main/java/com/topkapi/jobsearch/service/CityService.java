cfbf08dd004ccaa78b1ebf8d4513a432c76c3d84
Enes Uçar <enesucar1414@gmail.com>
2023-01-04 03:44:24 +0300

    City için servis yazıldı.

---
+++ src/main/java/com/topkapi/jobsearch/service/CityService.java
@@ -0,0 +1,21 @@
+package com.topkapi.jobsearch.service;
+
+import com.topkapi.jobsearch.exception.EntityNotFoundException;
+import com.topkapi.jobsearch.model.City;
+import com.topkapi.jobsearch.repository.CityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityService {
+    private final CityRepository cityRepository;
+
+    public CityService(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+    
+    protected City findById(String id) {
+        City city =  this.cityRepository.findById(id)
+                        .orElseThrow(() -> new EntityNotFoundException("City is not found: " + id));
+        return city;
+    }
+}
