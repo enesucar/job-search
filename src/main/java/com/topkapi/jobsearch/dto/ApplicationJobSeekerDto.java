046e7f10d345f35d712d5913b443061412f0d6c5
Enes Uçar <enesucar1414@gmail.com>
2023-01-04 23:24:18 +0300

    ApplicationJobSeekerDto oluşturuldu

---
+++ src/main/java/com/topkapi/jobsearch/dto/ApplicationJobSeekerDto.java
@@ -0,0 +1,16 @@
+package com.topkapi.jobsearch.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicationJobSeekerDto {
+    private String id;
+    private String fullName;
+    private String email;
+}
